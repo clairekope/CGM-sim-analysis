ead4c8bf5d697cb0973519aa6cdf47836293bbed
Claire Kopenhafer <kopenhaf@msu.edu>
2020-07-31 16:33:18 -0500

    auto load latest output

--- calc_sfr.py
+++ calc_sfr.py
@@ -1,21 +1,18 @@
 import yt
+import glob
 import numpy as np
 import matplotlib; matplotlib.use('agg')
 import matplotlib.pyplot as plt
 yt.enable_parallelism()
 
-#ds = yt.load("DD0601/DD0601")
-ds = yt.load("DD0583/DD0583")
+latest_output = sorted(glob.glob("DD????/DD????"))[-1]
 
-#for ds in dataset.piter():
-#    if int(ds.basename[-4:]) % 10 == 0 and int(ds.basename[-4]) > 0:
+ds = yt.load(latest_output)
 
 dsk = ds.disk([0.5,0.5,0.5], [0,0,1], (24.5,'kpc'), (2.275, 'kpc'))
-#try:
-masses = dsk['particle_mass'].in_units('Msun')
+
+masses = dsk['particle_mass'].in_units('Msun') # this doesn't compensate for SNe losses
 formation_time = dsk['creation_time'].in_units('yr')
-#except yt.utilities.exceptions.YTFieldNotFound:
-#    pass
         
 time_range = [0, 2e10] # years
 n_bins = 10000
@@ -31,5 +28,5 @@ sfr[sfr == 0] = np.nan
 plt.plot(time/1e6, sfr)
 plt.xlabel('Time  [Myr]')
 plt.ylabel('SFR  [M$_\odot$ yr$^{-1}$]')
-plt.savefig("SM6_sfr.png")
+plt.savefig("sfr.png")
 
