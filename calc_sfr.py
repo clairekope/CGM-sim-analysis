96f90384bc82c2e503a5a46863024e622762fcb4
Claire Kopenhafer <kopenhaf@msu.edu>
2020-12-09 14:06:23 -0500

    Adapted calc_sfr into a function

--- calc_sfr.py
+++ calc_sfr.py
@@ -15,32 +15,38 @@ def _initial_mass(field, data):
 
 yt.add_field('particle_initial_mass', function=_initial_mass, units='g')
 
-latest_output = sorted(glob.glob("DD????/DD????"))[-1]
+def calc_sfr(obj, year_bounds, n_bins):
 
-ds = yt.load(latest_output)
-
-dsk = ds.disk([0.5,0.5,0.5], [0,0,1], (24.5,'kpc'), (2.275, 'kpc'))
-
-masses = dsk['particle_initial_mass'].in_units('Msun')
-formation_time = dsk['creation_time'].in_units('yr')
+    masses = obj['particle_initial_mass'].in_units('Msun')
+    formation_time = obj['creation_time'].in_units('yr')
         
-time_range = [0, 2e10] # years
-n_bins = 10000
-hist, bins = np.histogram(formation_time, bins=n_bins, range=time_range,)
-inds = np.digitize(formation_time, bins=bins) # what bin does each time fall in?
-time = (bins[:-1] + bins[1:])/2 # center of bin
+    hist, bins = np.histogram(formation_time, bins=n_bins, range=year_bounds)
+    inds = np.digitize(formation_time, bins=bins) # what bin does each time fall in?
+    time = (bins[:-1] + bins[1:])/2 # center of bin
+    
+    # Sum masses formed at approx the same time; divide by size of time bins
+    sfr = np.array([masses[inds == j].sum()/(bins[j+1]-bins[j])
+                    for j in range(len(time))])
+    sfr[sfr == 0] = np.nan
 
-# Sum masses formed at approx the same time; divide by size of time bins
-sfr = np.array([masses[inds == j].sum()/(bins[j+1]-bins[j])
-                for j in range(len(time))])
-sfr[sfr == 0] = np.nan
+    return time, sfr
 
-cwd = os.getcwd()
+if __name__=="__main__":
 
-plt.plot(time/1e6, sfr)
-plt.ylim(0, 10)
-plt.xlabel('Time  [Myr]')
-plt.ylabel('SFR  [M$_\odot$ yr$^{-1}$]')
-plt.title(os.path.basename(cwd))
-plt.savefig("sfr.png")
+    latest_output = sorted(glob.glob("DD????/DD????"))[-1]
+
+    ds = yt.load(latest_output)
+
+    dsk = ds.disk([0.5,0.5,0.5], [0,0,1], (24.5,'kpc'), (2.275, 'kpc'))
+
+    time, sfr = calc_sfr(dsk, (0, 2e10), 10000) 
+
+    cwd = os.getcwd()
+
+    plt.plot(time/1e6, sfr)
+    plt.ylim(0, 10)
+    plt.xlabel('Time  [Myr]')
+    plt.ylabel('SFR  [M$_\odot$ yr$^{-1}$]')
+    plt.title(os.path.basename(cwd))
+    plt.savefig("sfr.png")
 
