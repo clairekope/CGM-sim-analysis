6d8fefe77239d2302cc44822e948bc21c6901382
Claire Kopenhafer <kopenhaf@msu.edu>
2021-01-19 16:24:20 -0500

    Added t_ff to cooling time v radius

--- plot_thermo_slabs.py
+++ plot_thermo_slabs.py
@@ -7,6 +7,7 @@ from matplotlib.ticker import MultipleLocator
 from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 from scipy.stats import binned_statistic
+from calc_enclosed_mass import *
 
 rcParams.update({'font.size': 14})
 
@@ -27,18 +28,25 @@ for ds in datasets.piter():
     for width, thickness, label in zip(widths, thicknesses, labels):
 
         fig, ax = plt.subplots(nrows=3, ncols=4, figsize=(24.5,15.5))
-        
+
+        # Thin slab is used for projections so I can avoid weighting by e.g. mass
         rect = ds.region([center, center, center],
                          [center-thickness/2, center-width/2, center-width/2],
                          [center+thickness/2, center+width/2, center+width/2])
         assert (rect.get_field_parameter("center") == center).all()
 
+        # Sphere is used for calculating average tff
+        sph = ds.sphere([center, center, center], width)
+
+        #
         # Projection Plots
+        #
         fields = ['density','temperature','pressure','entropy',
                   'radial_velocity','cooling_time']
         p = yt.ProjectionPlot(ds, 'x', fields, width=width,
                               data_source=rect, weight_field ='ones')
 
+        # Extract fixed resolution buffers
         p_frb = p.data_source.to_frb(width, 512)
 
         d_arr = np.array(p_frb['density'])
@@ -48,6 +56,7 @@ for ds in datasets.piter():
         v_arr = np.array(p_frb['radial_velocity']) / 1e5 # km/s
         c_arr = np.array(p_frb['cooling_time']) / 3.16887e8 / 1e9 # Gyr
 
+        # Plot FRBs
         extent = (-width/2, width/2, -width/2, width/2)
     
         d_im = ax[0,0].imshow(d_arr, origin='lower', norm=LogNorm(1e-32,1e-24), 
@@ -70,6 +79,7 @@ for ds in datasets.piter():
         c_im = ax[1,2].imshow(c_arr, origin='lower', norm=LogNorm(1e-2,1e4),
                               extent=extent, cmap='twilight')
 
+        # Add colorbars
         d_cb = fig.colorbar(d_im, ax=ax[0,0], pad=0.01, shrink=0.9)
         t_cb = fig.colorbar(t_im, ax=ax[1,0], pad=0.01, shrink=0.9)
         p_cb = fig.colorbar(p_im, ax=ax[0,1], pad=0.01, shrink=0.9)
@@ -77,6 +87,7 @@ for ds in datasets.piter():
         v_cb = fig.colorbar(v_im, ax=ax[0,2], pad=0.01, shrink=0.9)
         c_cb = fig.colorbar(c_im, ax=ax[1,2], pad=0.01, shrink=0.9)
 
+        # Add colorbar labels
         d_cb.set_label(r'< Density > [g cm$^{-3}$]')
         t_cb.set_label(r'< Temperature > [K]')
         p_cb.set_label(r'< Pressure > [dyn cm$^{-2}$]')
@@ -84,7 +95,9 @@ for ds in datasets.piter():
         v_cb.set_label(r'< Radial Velocity > [km/s]')
         c_cb.set_label(r'< Cooling Time > [Gyr]')
 
+        #
         # Phase Diagrams
+        #
         dt_ph = yt.PhasePlot(rect, 'density', 'temperature', 'cell_mass').profile
         pk_ph = yt.PhasePlot(rect, 'pressure', 'entropy', 'cell_mass').profile
         rc_ph = yt.PhasePlot(rect, 'radius', 'cooling_time', 'cell_mass').profile
@@ -132,7 +145,20 @@ for ds in datasets.piter():
         rk_84 = binned_statistic(rect['radius'].to('kpc'), rect['entropy'],
                                  statistic=lambda y: np.percentile(y,84),
                                  bins=rk_ph.x_bins.to('kpc'))
+
+        # Calculate tff for tcool vs tff plot
+        # tff is calculated for material enclosed within a sphere
+        # as MN_accel averages the stellar potential over samples of a sphere
+        # (technically of a circle b/c of azimuthal symmetry)
+        Mtot_r = NFW_mass_enclosed(rc_ph.x) + \
+                 cell_and_particle_mass_enclosed(sph, rc_ph.x_bins.v)
+
+        g_r = yt.units.G * Mtot_r / np.power(rc_ph.x, 2)
+        g_r = g_r.to('cm/s**2') + MN_accel(rc_ph.x)
+
+        t_ff = np.sqrt( 2*rc_ph.x / g_r )
         
+        # Plot phase diagrams
         dt_im = ax[2,0].pcolormesh(dt_ph.x_bins, dt_ph.y_bins,
                                    dt_ph['cell_mass'].T.to('Msun'),
                                    norm=LogNorm(1e-4,1e6), cmap='viridis')
@@ -149,6 +175,7 @@ for ds in datasets.piter():
                                    rk_ph['cell_mass'].T.to('Msun'),
                                    norm=LogNorm(), cmap='magma')
 
+        # Plot percentiles
         ax[2,0].plot(dt_ph.x, dt_med[0], 'k-')
         ax[2,0].plot(dt_ph.x, dt_16[0], 'k:')
         ax[2,0].plot(dt_ph.x, dt_84[0], 'k:')
@@ -165,6 +192,11 @@ for ds in datasets.piter():
         ax[2,3].plot(rk_ph.x.to('kpc'), rk_16[0], 'k:')
         ax[2,3].plot(rk_ph.x.to('kpc'), rk_84[0], 'k:')
 
+        # Plot tff
+        ax[2,2].plot(rc_ph.x.to('kpc'), t_ff.to('Gyr'), 'g--')
+        ax[2,2].plot(rc_ph.x.to('kpc'), 10*t_ff.to('Gyr'), 'g-.')
+
+        # Adjust limits
         ax[2,0].set_xlim(1e-34, 1e-21)
         ax[2,0].set_xlabel(r'Density [g cm$^{-3}$]')
         ax[2,0].set_ylim(1e1, 1e9)
@@ -184,7 +216,8 @@ for ds in datasets.piter():
         ax[2,3].set_xlabel('Radius [kpc]')
         #ax[2,3].set_ylim()
         ax[2,3].set_ylabel(r'Entropy [keV cm$^2$]')
-        
+
+        # Add colorbars and labels
         dt_cb = fig.colorbar(dt_im, ax=ax[2,0], pad=0.01, shrink=0.9)
         pk_cb = fig.colorbar(pk_im, ax=ax[2,1], pad=0.01, shrink=0.9)
         rc_cb = fig.colorbar(rc_im, ax=ax[2,2], pad=0.01, shrink=0.9)
@@ -194,15 +227,21 @@ for ds in datasets.piter():
         pk_cb.set_label(r'Cell Mass [M$_\odot$]')
         rc_cb.set_label(r'Cell Mass [M$_\odot$]')
         rk_cb.set_label(r'Cell Mass [M$_\odot$]')
-        
+
+        # Set plot view stuff
         for i in range(4):
             ax[2,i].set_xscale('log')
             ax[2,i].set_yscale('log')
             ax[2,i].set_aspect( 1.0 / ax[2,i].get_data_ratio() )
 
+        #
         # Plots over time
+        #
+
+        # Translate output number to index
         index = int(ds.basename[-4:])
 
+        # Plot SFR up to this point
         time = masses[index,0]
         past_sfr = sfr[:,0] <= time
         ax[0,3].plot(sfr[past_sfr,0], sfr[past_sfr, 1], 'k')
@@ -210,7 +249,8 @@ for ds in datasets.piter():
         ax[0,3].set_xlabel('Time [Myr]')
         ax[0,3].set_ylim(0, 47)
         ax[0,3].set_ylabel(r'SFR [M$_\odot$/yr]')
-        
+
+        # Plot sum of SFR up to this point
         sum_ax = ax[0,3].twinx()
         sum_ax.plot(sfr[past_sfr,0], np.nancumsum(sfr[past_sfr, 1]),
                     color='C0', ls='--')
@@ -221,20 +261,23 @@ for ds in datasets.piter():
         ax[0,3].xaxis.tick_top()
         ax[0,3].xaxis.set_label_position('top')
         ax[0,3].yaxis.set_major_locator(MultipleLocator(5))
-        
+
+        # Plot disk masses up to this point
         ax[1,3].plot(masses[:index,0], masses[:index,1],
                      ls='--', label=r'$\rm M_{disk}$')
         ax[1,3].plot(masses[:index,0], masses[:index,2],
                      ls='-.', label=r'$\rm M_\ast$')
         ax[1,3].plot(masses[:index,0], masses[:index,3],
-                     ls='-', label=r'$\rm M_{tot}$')       
+                     ls='-', label=r'$\rm M_{sum}$')       
         ax[1,3].set_xlim(0, 6000)
         ax[1,3].set_xlabel('Time [Myr]')
         ax[1,3].set_ylim(3e9, 8e9)
         ax[1,3].set_ylabel(r'Mass [M$_\odot$]')
         ax[1,3].legend()
-            
+
+        #
         # Final details & save
+        #
         ax[0,0].set_xlabel('y (kpc)')
         ax[0,0].set_ylabel('z (kpc)')
         
