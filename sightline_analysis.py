45b8f66a05629b0b89fc40af3a290f5035996148
Claire Kopenhafer <kopenhaf@msu.edu>
2021-03-30 10:07:14 -0400

    Reattempt dataset loads using subprocesses to avoid segfault

--- sightline_analysis.py
+++ sightline_analysis.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 import yt
-yt.enable_parallelism()
+yt.funcs.mylog.setLevel(50) # disable logging
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
-import glob
+import sys
 
+#
+# User Settings
+#
 n_theta = 13
 phi_step = 10
 fields = ['entropy','cooling_time','density','temperature','metallicity','pressure','cell_mass']
 units = ['keV*cm**2','Gyr','g*cm**-3','K','Zsun','dyne*cm**-2','Msun']
 
+
+#
+# Global Variables
+#
 
 # currently limit theta to upper hemisphere
 theta = np.linspace(0, 180, n_theta, endpoint=True)
@@ -22,119 +28,98 @@ n_phi = phi.size
 # each row is const phi
 theta_coord, phi_coord = np.meshgrid(np.deg2rad(theta), np.deg2rad(phi))
 
-results = {}
+#
+# Work functions
+#
 
-filenames = glob.glob("DD??[0-8]?/DD????")
-for sto, filename in yt.parallel_objects(filenames, 0, dynamic=False, storage=results):
-
+def process_dataset(filename):
     ds = yt.load(filename)
-    rays_ok = False
+        
+    length = ds.quan(206, 'kpc')
+    s = 0.5 * np.ones((3, n_phi, n_theta)) # dim 1 is const phi; dim 0 is x,y,z
+    starts = ds.arr(s, 'code_length')
+    uvec = np.array([np.sin(theta_coord)*np.cos(phi_coord),
+                     np.sin(theta_coord)*np.sin(phi_coord),
+                     np.cos(theta_coord)])
+    ends = starts + length*uvec
 
-    while not rays_ok:
+    # each list within rays will have constant theta
+    rays = []
+
+    # the phi angles for i_theta==0 and i_theta==n_theta-1 are all redundant
+    # as these are the poles
+    rays.append(ds.ray(starts[:, 0, 0], ends[:, 0, 0]))
+    for i_theta in range(1,n_theta-1):
+        rays.append([])
+        for i_phi in range(n_phi):
+            rays[i_theta].append(ds.ray(starts[:, i_phi, i_theta], 
+                                        ends[:, i_phi, i_theta]))
+    rays.append(ds.ray(starts[:, -1, -1], ends[:, -1, -1]))
+
+    r_edges = np.logspace(np.log10(2e-1), np.log10(206), 21)
+    r_centers = r_edges[:-1] + np.diff(r_edges)/2
+
+    quantity_arrays = {}
     
-        length = ds.quan(206, 'kpc')
-        s = 0.5 * np.ones((3, n_phi, n_theta)) # dim 1 is const phi; dim 0 is x,y,z
-        starts = ds.arr(s, 'code_length')
-        uvec = np.array([np.sin(theta_coord)*np.cos(phi_coord),
-                         np.sin(theta_coord)*np.sin(phi_coord),
-                         np.cos(theta_coord)])
-        ends = starts + length*uvec
-
-        # each list within rays will have constant theta
-        rays = []
-
-        # the phi angles for i_theta==0 and i_theta==n_theta-1 are all redundant
-        # as these are the poles
-        rays.append(ds.ray(starts[:, 0, 0], ends[:, 0, 0]))
-        for i_theta in range(1,n_theta-1):
-            rays.append([])
-            for i_phi in range(n_phi):
-                rays[i_theta].append(ds.ray(starts[:, i_phi, i_theta], 
-                                            ends[:, i_phi, i_theta]))
-        rays.append(ds.ray(starts[:, -1, -1], ends[:, -1, -1]))
-
-        # # Check for problems
-        # try:
-        #     rays[0]['density']
-        #     rays[-1]['density']
-        #     for lst in rays[1:-1]:
-        #         for ray in lst:
-        #             ray['density']
-        #     rays_ok = True
-        # except ValueError:
-        #     print("Reloading", ds.basename, flush=True)
-        #     ds.close()
-        #     ds = yt.load(filename)
-    
-        r_edges = np.logspace(np.log10(2e-1), np.log10(206), 21)
-        r_centers = r_edges[:-1] + np.diff(r_edges)/2
-
-        quantity_arrays = {}
-
-        try:
-            # Is this the most efficient outer loop? heck no
-            # It should really be the innermost loop
-            # But it's easy to program
-            for quantity_name, unit in zip(fields, units):
+    # Is this the most efficient outer loop? heck no
+    # It should really be the innermost loop
+    # But it's easy to program
+    for quantity_name, unit in zip(fields, units):
             
-                quantity_arrays[quantity_name] = {}
+        quantity_arrays[quantity_name] = {}
 
-                # Bin quantity of all rays with matching theta
-                for i_theta in range(0,n_theta):
+        # Bin quantity of all rays with matching theta
+        for i_theta in range(0,n_theta):
 
-                    quantities = []
+            quantities = []
 
-                    # Theta = 0 or 2pi
-                    if i_theta==0 or i_theta==n_theta-1:
-                        ray = rays[0]
-                        r_binner = np.digitize(ray['radius'].to('kpc'), r_edges)
-                        for i in range(1, r_edges.size):
-                            this_bin = r_binner==i
-                            quantities.append(list(ray[quantity_name][this_bin].to(unit).value))
+            # Theta = 0 or 2pi
+            if i_theta==0 or i_theta==n_theta-1:
+                ray = rays[0]
+                r_binner = np.digitize(ray['radius'].to('kpc'), r_edges)
+                for i in range(1, r_edges.size):
+                    this_bin = r_binner==i
+                    quantities.append(list(ray[quantity_name][this_bin].to(unit).value))
 
-                    else:
-                        # first ray at this theta 
-                        ray = rays[i_theta][0]    
-                        r_binner = np.digitize(ray['radius'].to('kpc'), r_edges)
-                        for i in range(1, r_edges.size):
-                            this_bin = r_binner==i
-                            quantities.append(list(ray[quantity_name][this_bin].to(unit).value))
+            else:
+                # first ray at this theta 
+                ray = rays[i_theta][0]    
+                r_binner = np.digitize(ray['radius'].to('kpc'), r_edges)
+                for i in range(1, r_edges.size):
+                    this_bin = r_binner==i
+                    quantities.append(list(ray[quantity_name][this_bin].to(unit).value))
 
-                        # subsequent rays at this theta
-                        for ray in rays[i_theta][1:]:
-                            r_binner = np.digitize(ray['radius'].to('kpc'), r_edges)
-                            for i in range(1, r_edges.size):
-                                this_bin = r_binner==i
-                                quantities[i-1].extend(ray[quantity_name][this_bin].to(unit).value)
+                # subsequent rays at this theta
+                for ray in rays[i_theta][1:]:
+                    r_binner = np.digitize(ray['radius'].to('kpc'), r_edges)
+                    for i in range(1, r_edges.size):
+                        this_bin = r_binner==i
+                        quantities[i-1].extend(ray[quantity_name][this_bin].to(unit).value)
 
-                    # Process/compress bins into med, min, and max
-                    quantity_min = np.zeros(len(quantities))
-                    quantity_low = np.zeros(len(quantities))
-                    quantity_med = np.zeros(len(quantities))
-                    quantity_upp = np.zeros(len(quantities))
-                    quantity_max = np.zeros(len(quantities))
+            # Process/compress bins into med, min, and max
+            quantity_min = np.zeros(len(quantities))
+            quantity_low = np.zeros(len(quantities))
+            quantity_med = np.zeros(len(quantities))
+            quantity_upp = np.zeros(len(quantities))
+            quantity_max = np.zeros(len(quantities))
 
-                    for i in range(len(quantities)):
-                        quantity_min[i] = np.min(quantities[i])
-                        quantity_low[i] = np.percentile(quantities[i], 16)
-                        quantity_med[i] = np.median(quantities[i])
-                        quantity_upp[i] = np.percentile(quantities[i], 84)
-                        quantity_max[i] = np.max(quantities[i])
+            for i in range(len(quantities)):
+                quantity_min[i] = np.min(quantities[i])
+                quantity_low[i] = np.percentile(quantities[i], 16)
+                quantity_med[i] = np.median(quantities[i])
+                quantity_upp[i] = np.percentile(quantities[i], 84)
+                quantity_max[i] = np.max(quantities[i])
 
-                    quantity_arrays[quantity_name][i_theta] = {'min':quantity_min,
-                                                               'p16':quantity_low,
-                                                               'med':quantity_med,
-                                                               'p84':quantity_upp,
-                                                               'max':quantity_max}
-            rays_ok = True
-        except ValueError:
-            print("Reloading", ds.basename, flush=True)
-            ds.close()
-            ds = yt.load(filename)
-            
-    sto.result = quantity_arrays
-    sto.result_id = ds.basename
+            quantity_arrays[quantity_name][i_theta] = {'min':quantity_min,
+                                                       'p16':quantity_low,
+                                                       'med':quantity_med,
+                                                       'p84':quantity_upp,
+                                                       'max':quantity_max}
 
-if yt.is_root():
-    with open("hedgehog_data.pkl","wb") as f:
-        pickle.dump(results, f, protocol=3)
+    return quantity_arrays
+
+if __name__=="__main__":
+    assert len(sys.argv) == 2
+    data = process_dataset(sys.argv[1])
+    with open(f"data_{sys.argv[1][-6:]}.pkl","wb") as f:
+        pickle.dump(data, f, protocol=3)
---
+++ sightline_analysis_subproc_manager.py
@@ -0,0 +1,35 @@
+import yt
+yt.enable_parallelism(suppress_logging=True)
+import glob
+import subprocess
+import os
+from mpi4py import MPI
+from sightline_analysis import process_dataset
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+def launch_subprocess(filename):
+    proc = subprocess.Popen(['python',
+                             '/home/claire/isolated_galaxies/CGM_sim_analysis/sightline_analysis.py',
+                             filename])
+    return proc
+
+
+filenames = glob.glob("DD??[0-8]?/DD????")
+for filename in yt.parallel_objects(filenames):
+
+    #print("Parent CPU:", os.sched_getaffinity(0))
+    
+    rays_bad = True
+
+    while rays_bad:
+        proc = launch_subprocess(filename)
+        print("Rank", rank, "launched", filename, "on pid", proc.pid, flush=True)
+
+        proc.wait() # wait for subprocces to finish
+
+        print("Rank", rank, "pip", proc.pid, "returncode:", proc.returncode, flush=True)
+        rays_bad = proc.returncode # anything but 0 is failure
+        
+    print("Rank", rank, "finished file", filename, flush=True)
