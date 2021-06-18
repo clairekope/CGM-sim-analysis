d9a683f7f587d211f61dae474690ea706b053465
Claire Kopenhafer <kopenhaf@msu.edu>
2021-06-18 13:03:03 -0400

    Adjusted plot_recent_stars to import filters

--- plot_recent_stars.py
+++ plot_recent_stars.py
@@ -39,49 +39,46 @@ def recent_stars(pfilter, data):
     
     return filter
 
+if __name__=="__main__":
+    datasets = yt.load("DD????/DD????")
+    for ds in datasets.piter(dynamic=False, ):
+        
+        stars = ('io', 'particle_type') in ds.field_list
+        if stars:
+            ds.add_particle_filter('new_stars')
+            ds.add_particle_filter('recent_stars')
 
-datasets = yt.load("DD????/DD????")
-for ds in datasets.piter(dynamic=False, ):
+        center = ds.quan(0.5,'code_length')
+        rs = ds.quan(3.5,'kpc')
 
-    stars = True
+        width = ds.quan(100,'kpc')
+        thickness = rs
 
-    try:
-        ds.add_particle_filter('new_stars')
-        ds.add_particle_filter('recent_stars')
-    except:
-        stars = False
+        # Edge on
+        rect = ds.region([center, center, center],
+                         [center-thickness/2, center-width/2, center-width/2],
+                         [center+thickness/2, center+width/2, center+width/2])
+        assert (rect.get_field_parameter("center") == center).all()
 
-    center = ds.quan(0.5,'code_length')
-    rs = ds.quan(3.5,'kpc')
+        p1 = yt.ProjectionPlot(ds, 'x', 'density', width=width,
+                               data_source=rect, weight_field ='ones')
+        p1.set_zlim("density", 1e-32, 1e-24)
+        if stars:
+            p1.annotate_particles(width=width, ptype='recent_stars', p_size=9, col='dimgray')
+            p1.annotate_particles(width=width, ptype='new_stars', p_size=9)
+        p1.save(ds.basename+"_edge_on_stars.png")
 
-    width = ds.quan(100,'kpc')
-    thickness = rs
+        # Face on
+        rect = ds.region([center, center, center],
+                         [center-width/2, center-width/2, center-thickness/2],
+                         [center+width/2, center+width/2, center+thickness/2])
+        assert (rect.get_field_parameter("center") == center).all()
 
-    # Edge on
-    rect = ds.region([center, center, center],
-                     [center-thickness/2, center-width/2, center-width/2],
-                     [center+thickness/2, center+width/2, center+width/2])
-    assert (rect.get_field_parameter("center") == center).all()
-
-    p1 = yt.ProjectionPlot(ds, 'x', 'density', width=width,
-                           data_source=rect, weight_field ='ones')
-    p1.set_zlim("density", 1e-32, 1e-24)
-    if stars:
-        p1.annotate_particles(width=width, ptype='recent_stars', p_size=9, col='dimgray')
-        p1.annotate_particles(width=width, ptype='new_stars', p_size=9)
-    p1.save(ds.basename+"_edge_on_stars.png")
-
-    # Face on
-    rect = ds.region([center, center, center],
-                     [center-width/2, center-width/2, center-thickness/2],
-                     [center+width/2, center+width/2, center+thickness/2])
-    assert (rect.get_field_parameter("center") == center).all()
-
-    p2 = yt.ProjectionPlot(ds, 'z', 'density', width=width,
-                           data_source=rect, weight_field ='ones')
-    p2.set_zlim("density", 1e-29, 1e-23)
-    if stars:
-        p2.annotate_particles(width=width, ptype='recent_stars', p_size=9, col='dimgray')
-        p2.annotate_particles(width=width, ptype='new_stars', p_size=9)
-    p2.save(ds.basename+"_face_on_stars.png")
+        p2 = yt.ProjectionPlot(ds, 'z', 'density', width=width,
+                               data_source=rect, weight_field ='ones')
+        p2.set_zlim("density", 1e-29, 1e-23)
+        if stars:
+            p2.annotate_particles(width=width, ptype='recent_stars', p_size=9, col='dimgray')
+            p2.annotate_particles(width=width, ptype='new_stars', p_size=9)
+        p2.save(ds.basename+"_face_on_stars.png")
 
