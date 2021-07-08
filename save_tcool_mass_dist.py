d33e23c6aa87d02d07eb0eb8c0884c4d186fa287
Claire Kopenhafer <kopenhaf@msu.edu>
2021-07-08 10:57:40 -0400

    Save tcool mass dist for CGM; use separate file

--- save_tcool_mass_dist.py
+++ save_tcool_mass_dist.py
@@ -14,7 +14,11 @@ storage = {}
 
 for my_storage, ds in datasets.piter(dynamic=False, storage=storage):
     sph = ds.sphere('c',(206,'kpc'))
-    prof = yt.create_profile(sph, "cooling_time", "cell_mass", 
+    dsk = ds.disk([0.5,0.5,0.5], [0,0,1], (20,'kpc'), (1.3, 'kpc'))
+    cgm = sph - dsk
+    cgm.set_field_parameter('center', ds.arr([0.5,0.5,0.5], 'code_length'))
+    
+    prof = yt.create_profile(cgm, "cooling_time", "cell_mass", 
                              accumulation=True,
                              units={'cooling_time':'Gyr','cell_mass':'Msun'},
                              extrema={"cooling_time":(1e-6, 1e6)})
@@ -27,5 +31,5 @@ if yt.is_root():
     for i in range(len(datasets)):
         data_arr[:,i+1] = storage[i]
         
-    np.savetxt("tcool_mass_dist.txt", data_arr,
+    np.savetxt("tcool_mass_dist_CGM.txt", data_arr,
                header="tcool [Gyr]  DD0000 mass [Msun]  DD0001 mass ... etc")
