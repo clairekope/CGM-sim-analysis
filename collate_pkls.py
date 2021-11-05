7998e5a29171579cd7551c6dd4d927456575ce1e
Claire Kopenhafer <kopenhaf@msu.edu>
2021-11-05 07:21:36 -0700

    Tweaks for errors

--- collate_pkls.py
+++ collate_pkls.py
@@ -7,14 +7,16 @@
 
 import pickle
 import glob
+import re
 
 pickles = glob.glob('*mass.pkl')
 
 data = {}
 
 for file in pickles:
+    name = re.search(r'DD\d+', file).group()
     with open(file, 'rb') as f:
-        data[file[-10:-4]] = pickle.load(f)
+        data[name] = pickle.load(f)
 
 with open("hedgehog_mass.pkl", "wb") as f:
     pickle.dump(data, f)
