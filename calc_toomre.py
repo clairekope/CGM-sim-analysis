5d7b9ce84d116b29f3cad19f639f0140b5ddb523
Claire Kopenhafer <kopenhaf@msu.edu>
2022-03-01 15:56:59 -0500

    Added surface density and the Big Q

--- calc_toomre.py
+++ calc_toomre.py
@@ -5,16 +5,19 @@ from unyt import accepts, returns
 
 class DiskModel():
 
-    @accepts(Mvir=udim.mass, Mstar=udim.mass, rs_star=udim.length, zs_star=udim.length, T_disk=udim.temperature)
+    @accepts(Mvir=udim.mass, Mstar=udim.mass,
+             M_disk=udim.mass, T_disk=udim.temperature,
+             r_scale=udim.length, z_scale=udim.length)
     def __init__(self, 
                  M_vir=1e12*u.Msun,
                  C=10, 
                  M_star=5.8e10*u.Msun,
-                 rs_star=3.5*u.kpc,
-                 zs_star=0.325*u.kpc,
-                 T_disk=1e5*u.K):
+                 M_disk=5e9*u.Msun,
+                 T_disk=1e5*u.K,
+                 r_scale=3.5*u.kpc,
+                 z_scale=0.325*u.kpc,):
         """
-        Class for calculating the Toomre criterion as a function of radius.
+        Class for calculating the Toomre parameter as a function of radius.
         Initializer sets some basic parameters about the disk model.
 
         M_vir (unyt quantity)
@@ -29,25 +32,30 @@ class DiskModel():
             Stellar mass for the disk potential.
             Default: 5.8e10 Msun
 
-        rs_star (unyt quantity)
-            Scale radius for the stellar disk potential.
-            Default: 3.5 kpc
-
-        zs_star (unyt quantity)
-            Scale height for the stellar disk potential.
-            Default: 0.325 kpc
+        M_disk (unyt quantity)
+            Mass of the gas disk used for defining the gas density profile.
+            Default: 5e9 Msun
 
         T_disk (unyt quantity)
-            Temperature of the gaseous disk. Assumed to be an ideal gas.
+            Temperature of the isothermal gaseous disk. Assumed to be an ideal gas.
             Default: 1e5 K
+
+        r_scale (unyt quantity)
+            Scale radius for the stellar disk potential & gas density.
+            Default: 3.5 kpc
+
+        z_scale (unyt quantity)
+            Scale height for the stellar disk potential & gas density.
+            Default: 0.325 kpc
         """
         
         self.M_vir = M_vir
         self.C = C
         self.M_star = M_star
-        self.rs_star = rs_star
-        self.zs_star = zs_star
+        self.M_disk = M_disk
         self.T_disk = T_disk
+        self.r_scale = r_scale
+        self.z_scale = z_scale
 
         self.rho_crit = 1.8788e-29*0.49 * u.g/u.cm**3
         self.Rvir = ( 3.0/(4.0*np.pi)*self.M_vir / (200.*self.rho_crit) )**(1./3.)
@@ -78,7 +86,7 @@ class DiskModel():
         g_NFW = u.G*self.M_NFW/radii**2 * (np.log(1+x) - x/(1+x))
         
         # Miyamoto & Nagai potential
-        k = self.rs_star + self.zs_star
+        k = self.r_scale + self.z_scale
 
         g_MN = u.G*self.M_star*radii / np.power( np.power(radii,2) + k**2, 3/2 )
 
@@ -105,12 +113,12 @@ class DiskModel():
         # NFW potential
         x = radii/self.Rs
 
-        dg_NFW = np.power(radii, -2) * (2*self.Rs +3*radii)/np.power(self.Rs+radii,2)
+        dg_NFW = np.power(radii, -2) * (2*self.Rs + 3*radii)/np.power(self.Rs+radii,2)
         dg_NFW -= 2*np.power(radii,-3)*np.log(1+x)
         dg_NFW *= u.G * self.M_NFW
 
         # Miyamoto & Nagai potential
-        k = self.rs_star + self.zs_star
+        k = self.r_scale + self.z_scale
         d = np.power(radii, 2) + k**2
 
         dg_MN = u.G*self.M_star * np.power(d, -3/2) * (1 - 3*np.power(radii, 2)/d)
@@ -119,7 +127,7 @@ class DiskModel():
 
     @accepts(radii=udim.length)
     @returns(1/udim.time)
-    def omega(self, radii):
+    def Omega(self, radii):
         """
         Rotation frequency Omega = v_phi/r, where v_phi = sqrt(r * g(r))
 
@@ -130,7 +138,7 @@ class DiskModel():
 
         Outputs:
         --------
-        omega (unyt array)
+        Omega (unyt array)
             Rotation frequency as a function of radius. Radians are implied.
         """
 
@@ -178,17 +186,17 @@ class DiskModel():
             Epicyclic frequency at supplied radii
         """
 
-        omega = self.omega(radii)
+        Omega = self.Omega(radii)
         deriv = self._epicyclic_derivative(radii)
 
-        return np.sqrt(2*omega/radii * deriv)
+        return np.sqrt(2*Omega/radii * deriv)
 
     @accepts(radii=udim.length)
     @returns(udim.length/udim.time)
     def c_sound(self, radii):
         """
-        Calculate the sound speed of an isothermal disk, 
-        assuming an ideal gas and mean molecular weight = 0.6 (primordial)
+        Calculate the sound speed of an isothermal disk (i.e., constant sound speed), 
+        assuming an ideal gas and mean molecular weight = 0.6 (primordial).
 
         Inputs:
         -------
@@ -205,15 +213,84 @@ class DiskModel():
 
         return np.sqrt(gamma * u.kb * self.T_disk / (0.6*u.mp))
 
+    @accepts(radii=udim.length)
+    @returns(udim.mass/udim.length**2)
+    def Sigma(self, radii):
+        """
+        Calculate the surface density of the disk profile in Tonnesen & Bryan 09.
+        This is a double-sech disk with a smoothing factor applied for cylindrical radii r > 24 kpc.
+        The double-sech profile makes it easy to integrate out the vertical (z) component.
+
+         Inputs:
+        -------
+        radii (unyt array)
+            Radii at which to calculate the acceleration's derivative
+
+        Outputs:
+        --------
+        Sigma (unyt array)
+            Surface density at the supplied radii
+        """
+
+        R = 24*u.kpc
+
+        surdens = self.M_disk/(8*self.r_scale**2) / np.cosh(radii/self.r_scale)
+        smooth = 0.5 * (1 + np.cos( np.pi * (radii-R)/(7.2*u.kpc) ))
+
+        # multiply radii > R by smoothing factor
+        # np.where strips units so reapply them
+        return np.where(radii < R, surdens, surdens * smooth) * surdens.units
+
+    @accepts(radii=udim.length)
+    @returns(udim.dimensionless)
+    def Q(self, radii):
+        """
+        Calculate the Toomre parameter for an isothermal disk of ideal gas.
+
+         Inputs:
+        -------
+        radii (unyt array)
+            Radii at which to calculate the acceleration's derivative
+
+        Outputs:
+        --------
+        Q (unyt array)
+            The Toomre parameter
+        """
+
+        kappa = self.kappa(radii) # Depends on the preceeding functions
+        cs = self.c_sound(radii)
+        Sigma = self.Sigma(radii)
+
+        return cs*kappa/(np.pi*u.G*Sigma)
+
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
 
     r = np.linspace(0,30) * u.kpc
-    dsk = DiskModel()
 
-    # Function decorators ensure return products have desired units
-    # dsk.g(r)
-    # dsk.dg_dr(r)
-    # dsk.omega(r)
-    # dsk._epicyclic_derivative(r)
-    kappa = dsk.kappa(r) # Depends on the preceeding functions
-    cs = dsk.c_sound(r)
+    # Function decorators ensure return products have desired units!
+    dsk = DiskModel()
+    Q_hot = dsk.Q(r)
+    
+    dsk2 = DiskModel(T_disk=1e4*u.K)
+    Q_cool = dsk2.Q(r)
+
+    dsk3 = DiskModel(T_disk=1e3*u.K)
+    Q_cold = dsk3.Q(r)
+
+    plt.semilogy(r, Q_hot, label="T=1e5 K", color='C1')
+    plt.semilogy(r, Q_cool, label="T=1e4 K", color='C0')
+    plt.semilogy(r, Q_cold, label="T=1e3 K", color='C2')
+
+    plt.axvline(28.5, color="gray", ls="--")
+    plt.axhline(1, color='gray', ls='--')
+
+    plt.ylim(0.1,1e3)
+    plt.xlim(0,30)
+
+    plt.ylabel("Q")
+    plt.xlabel("r [kpc]")
+
+    plt.legend()
+    plt.savefig("toomre.png")
