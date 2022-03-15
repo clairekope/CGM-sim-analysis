bf6322681970447dd1051dee68162038b67fb197
Claire Kopenhafer <kopenhaf@msu.edu>
2022-03-15 12:56:36 -0400

    Revert tcool mass dist to original

--- save_tcool_mass_dist.py
+++ save_tcool_mass_dist.py
@@ -14,7 +14,7 @@ storage = {}
 
 for my_storage, ds in datasets.piter(dynamic=False, storage=storage):
     sph = ds.sphere('c',(206,'kpc'))
-    dsk = ds.disk([0.5,0.5,0.5], [0,0,1], (15,'kpc'), (1.3, 'kpc'))
+    dsk = ds.disk([0.5,0.5,0.5], [0,0,1], (20,'kpc'), (1.3, 'kpc'))
     cgm = sph - dsk
     cgm.set_field_parameter('center', ds.arr([0.5,0.5,0.5], 'code_length'))
     
@@ -31,5 +31,6 @@ if yt.is_root():
     for i in range(len(datasets)):
         data_arr[:,i+1] = storage[i]
         
-    np.savetxt("tcool_mass_dist_CGM_tight.txt", data_arr,
+    np.savetxt("tcool_mass_dist_CGM.txt", data_arr,
                header="tcool [Gyr]  DD0000 mass [Msun]  DD0001 mass ... etc")
+
