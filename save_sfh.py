e507424dfc025d11621409e9dbca3542583946be
Claire Kopenhafer <kopenhaf@msu.edu>
2022-03-15 13:20:27 -0400

    Updated save_sfh for calc_sfr changes

--- save_sfh.py
+++ save_sfh.py
@@ -6,12 +6,20 @@
 import glob
 import yt
 import numpy as np
-from calc_sfr import calc_sfr
+import calc_sfr
 
 latest_output = sorted(glob.glob("DD????/DD????"))[-1]
 ds = yt.load(latest_output)
 
 bins = np.linspace(0, 6e9, 10000)
+
+if all([field in ds.field_list for field in calc_sfr.required_fields]):
+
+    ds.add_field(name=('io','particle_initial_mass'),
+                 units='g',
+                 sampling_type='particle',
+                 function=calc_sfr.field_initial_mass)
+
 time, sfr = calc_sfr(ds.all_data(),bins)
 
 np.savetxt('sfr.txt',np.column_stack((time/1e6,sfr)),header='Time_Myr SFR_Msun/Yr')
