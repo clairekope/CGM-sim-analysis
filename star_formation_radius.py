4852eecf68b6ae3a632f86f4529969943b94b745
Claire Kopenhafer <kopenhaf@msu.edu>
2022-03-24 14:15:36 -0400

    Clarified size comparisons

--- star_formation_radius.py
+++ star_formation_radius.py
@@ -23,112 +23,97 @@ stds = np.array([np.std(fid[40:81]), np.std(cflow[40:81]),
       np.std(tctff5[40:81]), np.std(tctff20[40:81]), 
       np.std(linrot[40:81]), np.std(norot[40:81])])
 
-tab = pd.DataFrame(data=np.column_stack((means,stds)), 
-                   index=['Fid','Cflow','TcTff5','TcTff20','LinRot','NoRot'],
-                   columns=['Mean','Std Dev'])
+d = {'Mean':means, 'Std Dev':stds, 'Colors':['C0','C3','C2','C1','C4','C5']}
 
+tab = pd.DataFrame(data=d, 
+                   index=['Fid','Cflow','TcTff5','TcTff20','LinRot','NoRot'])
+
+tab = tab.sort_values(by='Mean', ascending=False)
 print(tab)
-
-plt.errorbar(tab.index, tab['Mean'], yerr=tab['Std Dev'], color='k',
-             ecolor=['C0','C3','C2','C1','C4','C5'], marker='o', ls='none')
-plt.minorticks_off()
-plt.ylabel('Max Star Formation Radius  [kpc]')
-
-plt.show()
 
 
 # # Significance Tests
 
-# ## vs Fiducial
-
-# ### Test 1: _larger_ than Fid
-
 # Level: $\alpha=0.05$. If True, you can reject the Null Hypothesis that the means are the same
+print('----------------------------------')
+
+print("Cflow larger than TcTff5:",
+tab['Mean']['TcTff5'] - tab['Mean']['Cflow'] < -1.645*np.sqrt(tab['Std Dev']['TcTff5']**2/41 + tab['Std Dev']['Cflow']**2/41))
+
+print("Cflow larger than TcTff20:",
+tab['Mean']['TcTff20'] - tab['Mean']['Cflow'] < -1.645*np.sqrt(tab['Std Dev']['TcTff20']**2/41 + tab['Std Dev']['Cflow']**2/41))
 
 print("Cflow larger than Fid:",
 tab['Mean']['Fid'] - tab['Mean']['Cflow'] < -1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['Cflow']**2/41))
 
+print("Cflow larger than LinRot:",
+tab['Mean']['LinRot'] - tab['Mean']['Cflow'] < -1.645*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['Cflow']**2/41))
+
+print("Cflow larger than NoRot:",
+tab['Mean']['NoRot'] - tab['Mean']['Cflow'] < -1.645*np.sqrt(tab['Std Dev']['NoRot']**2/41 + tab['Std Dev']['Cflow']**2/41))
+
+print('----------------------------------')
+
 print("TcTff5 larger than Fid:",
 tab['Mean']['Fid'] - tab['Mean']['TcTff5'] < -1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['TcTff5']**2/41))
 
-print("TcTff20 larger than Fid:",
-tab['Mean']['Fid'] - tab['Mean']['TcTff20'] < -1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['TcTff20']**2/41))
+print("TcTff5 larger than TcTff20:",
+tab['Mean']['TcTff20'] - tab['Mean']['TcTff5'] < -1.645*np.sqrt(tab['Std Dev']['TcTff20']**2/41 + tab['Std Dev']['TcTff5']**2/41))
+
+print("TcTff5 larger than LinRot:",
+tab['Mean']['LinRot'] - tab['Mean']['TcTff5'] < -1.645*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['TcTff5']**2/41))
+
+print("TcTff5 larger than NoRot:",
+tab['Mean']['NoRot'] - tab['Mean']['TcTff5'] < -1.645*np.sqrt(tab['Std Dev']['NoRot']**2/41 + tab['Std Dev']['TcTff5']**2/41))
+
+print("TcTff5 not equal to LinRot:",
+np.abs(tab['Mean']['TcTff5'] - tab['Mean']['LinRot']) >= 1.96*np.sqrt(tab['Std Dev']['TcTff5']**2/41 + tab['Std Dev']['LinRot']**2/41))
+
+print("TcTff5 not equal to NoRot:",
+np.abs(tab['Mean']['TcTff5'] - tab['Mean']['NoRot']) >= 1.96*np.sqrt(tab['Std Dev']['TcTff5']**2/41 + tab['Std Dev']['NoRot']**2/41))
+
+print('----------------------------------')
+
+print("LinRot larger than TcTff20:",
+tab['Mean']['TcTff20'] - tab['Mean']['LinRot'] < -1.645*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['TcTff20']**2/41))
 
 print("LinRot larger than Fid:",
 tab['Mean']['Fid'] - tab['Mean']['LinRot'] < -1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['LinRot']**2/41))
 
-print("NoRot larger than Fid:",
-tab['Mean']['Fid'] - tab['Mean']['NoRot'] < -1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['NoRot']**2/41))
-
-
-# ### Test 2: _smaller_ than Fid
-
-print("TcTff20 smaller than Fid:",
-tab['Mean']['Fid'] - tab['Mean']['TcTff20'] > 1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['TcTff20']**2/41))
-
-print("LinRot smaller than Fid:",
-tab['Mean']['Fid'] - tab['Mean']['LinRot'] > 1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['LinRot']**2/41))
-
-print("NoRot smaller than Fid:",
-tab['Mean']['Fid'] - tab['Mean']['NoRot'] > 1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['NoRot']**2/41))
-
-# ### Test 3: _not equal_ to Fid
-
-print("TcTff20 not equal to Fid:",
-np.abs(tab['Mean']['Fid'] - tab['Mean']['TcTff20']) >= 1.96*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['TcTff20']**2/41))
+print("LinRot larger than NoRot:",
+tab['Mean']['NoRot'] - tab['Mean']['LinRot'] < -1.645*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['NoRot']**2/41))
 
 print("LinRot not equal to Fid:",
 np.abs(tab['Mean']['Fid'] - tab['Mean']['LinRot']) >= 1.96*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['LinRot']**2/41))
-
-print("NoRot not equal to Fid:",
-np.abs(tab['Mean']['Fid'] - tab['Mean']['NoRot']) >= 1.96*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['NoRot']**2/41))
-
-print('----------------------------------')
-
-
-
-# ## Cflow vs TcTff5
-
-print("Cflow larger than TcTff5:",
-tab['Mean']['Cflow'] - tab['Mean']['TcTff5'] < -1.645*np.sqrt(tab['Std Dev']['Cflow']**2/41 + tab['Std Dev']['TcTff5']**2/41))
-
-print("Cflow not equal to TcTff5:",
-np.abs(tab['Mean']['Cflow'] - tab['Mean']['TcTff5']) >= 1.96*np.sqrt(tab['Std Dev']['Cflow']**2/41 + tab['Std Dev']['TcTff5']**2/41))
-
-print("TcTff5 smaller than Cflow:",
-np.abs(tab['Mean']['TcTff5'] - tab['Mean']['Cflow']) > 1.645*np.sqrt(tab['Std Dev']['TcTff5']**2/41 + tab['Std Dev']['Cflow']**2/41))
-
-print('----------------------------------')
-
-
-
-# ## vs NoRot
-
-print("LinRot larger than NoRot:",
-tab['Mean']['LinRot'] - tab['Mean']['NoRot'] > 1.645*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['NoRot']**2/41))
-
-print("NoRot smaller than LinRot:",
-tab['Mean']['NoRot'] - tab['Mean']['LinRot'] > 1.645*np.sqrt(tab['Std Dev']['NoRot']**2/41 + tab['Std Dev']['LinRot']**2/41))
 
 print("LinRot not equal to NoRot:",
 np.abs(tab['Mean']['LinRot'] - tab['Mean']['NoRot']) >= 1.96*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['NoRot']**2/41))
 
 print('----------------------------------')
 
-
-
-# ## vs TcTff20
-
 print("NoRot larger than TcTff20:",
-tab['Mean']['NoRot'] - tab['Mean']['TcTff20'] < -1.645*np.sqrt(tab['Std Dev']['NoRot']**2/41 + tab['Std Dev']['TcTff20']**2/41))
+tab['Mean']['TcTff20'] - tab['Mean']['NoRot'] < -1.645*np.sqrt(tab['Std Dev']['NoRot']**2/41 + tab['Std Dev']['TcTff20']**2/41))
 
-print("LinRot larger than TcTff20:",
-tab['Mean']['LinRot'] - tab['Mean']['TcTff20'] < -1.645*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['TcTff20']**2/41))
+print("NoRot larger than Fid:",
+tab['Mean']['Fid'] - tab['Mean']['NoRot'] < -1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['NoRot']**2/41))
 
-print("NoRot not equal to TcTff20:",
-np.abs(tab['Mean']['NoRot'] - tab['Mean']['TcTff20']) >= 1.96*np.sqrt(tab['Std Dev']['NoRot']**2/41 + tab['Std Dev']['TcTff20']**2/41))
+print("NoRot larger than LinRot:",
+tab['Mean']['LinRot'] - tab['Mean']['NoRot'] < -1.645*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['NoRot']**2/41))
 
-print("LinRot not equal to TcTff20:",
-np.abs(tab['Mean']['LinRot'] - tab['Mean']['TcTff20']) >= 1.96*np.sqrt(tab['Std Dev']['LinRot']**2/41 + tab['Std Dev']['TcTff20']**2/41))
+print("NoRot not equal to Fid:",
+np.abs(tab['Mean']['Fid'] - tab['Mean']['NoRot']) >= 1.96*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['NoRot']**2/41))
 
 print('----------------------------------')
+
+print("Fid larger than TcTff20:",
+tab['Mean']['TcTff20'] - tab['Mean']['Fid'] < -1.645*np.sqrt(tab['Std Dev']['Fid']**2/41 + tab['Std Dev']['TcTff20']**2/41))
+
+print('----------------------------------')
+
+plt.errorbar(tab.index, tab['Mean'], yerr=tab['Std Dev'], color='k',
+             ecolor=tab['Colors'], marker='o', ls='none')
+plt.minorticks_off()
+plt.ylabel('Max Star Formation Radius  [kpc]')
+
+plt.show()
+
