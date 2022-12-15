6f469d41a66b6e3f45c7ecafc9010ee85298ddf9
Claire Kopenhafer <kopenhaf@msu.edu>
2022-12-14 21:59:15 -0500

    Change stellar mass binning on figures

--- plot_disk_mass_evolution.py
+++ plot_disk_mass_evolution.py
@@ -15,24 +15,12 @@ linrot_mass = np.genfromtxt("../original_sims/extracted_data/linrot_masses_over_
 norot_mass = np.genfromtxt("../original_sims/extracted_data/norot_masses_over_time.txt", names=True)
 
 # Load star formation history
-fid_sfh = pd.read_csv('../original_sims/extracted_data/fid_sfh.txt', header=0, index_col=0,
-                      usecols=[1,0], names=['t_Myr','SFR'], delimiter=' ',
-                      converters={'t_Myr': lambda x: float(x)/1000}).fillna(0)
-cflow_sfh = pd.read_csv('../original_sims/extracted_data/cflow_sfh.txt', header=0, index_col=0,
-                        usecols=[1,0], names=['t_Myr','SFR'], delimiter=' ',
-                        converters={'t_Myr': lambda x: float(x)/1000}).fillna(0)
-tctff5_sfh = pd.read_csv('../original_sims/extracted_data/tctff5_sfh.txt', header=0, index_col=0,
-                         usecols=[1,0], names=['t_Myr','SFR'], delimiter=' ',
-                         converters={'t_Myr': lambda x: float(x)/1000}).fillna(0)
-tctff20_sfh = pd.read_csv('../original_sims/extracted_data/tctff20_sfh.txt', header=0, index_col=0,
-                          usecols=[1,0], names=['t_Myr','SFR'], delimiter=' ',
-                          converters={'t_Myr': lambda x: float(x)/1000}).fillna(0)
-linrot_sfh = pd.read_csv('../original_sims/extracted_data/linrot_sfh.txt', header=0, index_col=0,
-                         usecols=[1,0], names=['t_Myr','SFR'], delimiter=' ',
-                         converters={'t_Myr': lambda x: float(x)/1000}).fillna(0)
-norot_sfh = pd.read_csv('../original_sims/extracted_data/norot_sfh.txt', header=0, index_col=0,
-                        usecols=[1,0], names=['t_Myr','SFR'], delimiter=' ',
-                        converters={'t_Myr': lambda x: float(x)/1000}).fillna(0)
+fid_sfh = np.genfromtxt('../original_sims/extracted_data/fid_coarse-bin_sfh.txt')
+cflow_sfh = np.genfromtxt('../original_sims/extracted_data/cflow_coarse-bin_sfh.txt')
+tctff5_sfh = np.genfromtxt('../original_sims/extracted_data/tctff5_coarse-bin_sfh.txt')
+tctff20_sfh = np.genfromtxt('../original_sims/extracted_data/tctff20_coarse-bin_sfh.txt')
+linrot_sfh = np.genfromtxt('../original_sims/extracted_data/linrot_coarse-bin_sfh.txt')
+norot_sfh = np.genfromtxt('../original_sims/extracted_data/norot_coarse-bin_sfh.txt')
 
 
 # Plot disk mass growth and SFH
@@ -51,32 +39,32 @@ for i in range(3):
     ax[0,i].plot(fid_mass['Time_Myr']/1000, (fid_mass['FormedMass_Msun']+fid_mass['DiskGas_Msun'])/1e9, color='C0', ls='-')
     ax[0,i].plot(fid_mass['Time_Myr']/1000, fid_mass['FormedMass_Msun']/1e9, color='C0', ls='--')
     ax[0,i].plot(fid_mass['Time_Myr']/1000, fid_mass['DiskGas_Msun']/1e9, color='C0', ls=':')
-    ax[1,i].plot(fid_sfh.rolling(window=window).mean(), color='C0', lw=1)
+    ax[1,i].plot(fid_sfh[:,0]/1000, fid_sfh[:,1], color='C0', lw=1)
 
 ax[0,0].plot(cflow_mass['Time_Myr']/1000, (cflow_mass['FormedMass_Msun']+cflow_mass['DiskGas_Msun'])/1e9, color='C3', ls='-')
 ax[0,0].plot(cflow_mass['Time_Myr']/1000, cflow_mass['FormedMass_Msun']/1e9, color='C3', ls='--')
 ax[0,0].plot(cflow_mass['Time_Myr']/1000, cflow_mass['DiskGas_Msun']/1e9, color='C3', ls=':')
-ax[1,0].plot(cflow_sfh.rolling(window=window).mean(), color='C3', lw=1)
+ax[1,0].plot(cflow_sfh[:,0]/1000, cflow_sfh[:,1], color='C3', lw=1)
 
 ax[0,1].plot(tctff5_mass['Time_Myr']/1000, (tctff5_mass['FormedMass_Msun']+tctff5_mass['DiskGas_Msun'])/1e9, color='C2', ls='-')
 ax[0,1].plot(tctff5_mass['Time_Myr']/1000, tctff5_mass['FormedMass_Msun']/1e9, color='C2', ls='--')
 ax[0,1].plot(tctff5_mass['Time_Myr']/1000, tctff5_mass['DiskGas_Msun']/1e9, color='C2', ls=':')
-ax[1,1].plot(tctff5_sfh.rolling(window=window).mean(), color='C2', lw=1)
+ax[1,1].plot(tctff5_sfh[:,0]/1000, tctff5_sfh[:,1], color='C2', lw=1)
 
 ax[0,1].plot(tctff20_mass['Time_Myr']/1000, (tctff20_mass['FormedMass_Msun']+tctff20_mass['DiskGas_Msun'])/1e9, color='C1', ls='-')
 ax[0,1].plot(tctff20_mass['Time_Myr']/1000, tctff20_mass['FormedMass_Msun']/1e9, color='C1', ls='--')
 ax[0,1].plot(tctff20_mass['Time_Myr']/1000, tctff20_mass['DiskGas_Msun']/1e9, color='C1', ls=':')
-ax[1,1].plot(tctff20_sfh.rolling(window=window).mean(), color='C1', lw=1)
+ax[1,1].plot(tctff20_sfh[:,0]/1000, tctff20_sfh[:,1], color='C1', lw=1)
 
 ax[0,2].plot(linrot_mass['Time_Myr']/1000, (linrot_mass['FormedMass_Msun']+linrot_mass['DiskGas_Msun'])/1e9, color='C4', ls='-')
 ax[0,2].plot(linrot_mass['Time_Myr']/1000, linrot_mass['FormedMass_Msun']/1e9, color='C4', ls='--')
 ax[0,2].plot(linrot_mass['Time_Myr']/1000, linrot_mass['DiskGas_Msun']/1e9, color='C4', ls=':')
-ax[1,2].plot(linrot_sfh.rolling(window=window).mean(), color='C4', lw=1)
+ax[1,2].plot(linrot_sfh[:,0]/1000, linrot_sfh[:,1], color='C4', lw=1)
 
 ax[0,2].plot(norot_mass['Time_Myr']/1000, (norot_mass['FormedMass_Msun']+norot_mass['DiskGas_Msun'])/1e9, color='C5', ls='-')
 ax[0,2].plot(norot_mass['Time_Myr']/1000, norot_mass['FormedMass_Msun']/1e9, color='C5', ls='--')
 ax[0,2].plot(norot_mass['Time_Myr']/1000, norot_mass['DiskGas_Msun']/1e9, color='C5', ls=':')
-ax[1,2].plot(norot_sfh.rolling(window=window).mean(), color='C5', lw=1)
+ax[1,2].plot(norot_sfh[:,0]/1000, norot_sfh[:,1], color='C5', lw=1)
 
 # Set up legend
 total = Line2D([0], [0], ls='-', c='dimgray')
@@ -212,32 +200,32 @@ norot_ref = norot_mass[ref]
 fid_late_time = fid_mass["Time_Myr"][ref:end]/1000
 fid_late_gas = fid_mass["DiskGas_Msun"][ref:end]-fid_ref["DiskGas_Msun"]
 fid_late_form = fid_mass["FormedMass_Msun"][ref:end]-fid_ref["FormedMass_Msun"]
-fid_late_sfr = fid_sfh[(fid_sfh.index >= 2) & (fid_sfh.index <= 4)]
+fid_late_sfr = fid_sfh[(fid_sfh[:,0]/1000 >= 2) & (fid_sfh[:,0]/1000 <= 4)]
 
 cflow_late_time = cflow_mass["Time_Myr"][ref:end]/1000
 cflow_late_gas = cflow_mass["DiskGas_Msun"][ref:end]-cflow_ref["DiskGas_Msun"]
 cflow_late_form = cflow_mass["FormedMass_Msun"][ref:end]-cflow_ref["FormedMass_Msun"]
-cflow_late_sfr = cflow_sfh[(cflow_sfh.index >= 2) & (cflow_sfh.index <= 4)]
+cflow_late_sfr = cflow_sfh[(cflow_sfh[:,0]/1000 >= 2) & (cflow_sfh[:,0]/1000 <= 4)]
 
 tctff5_late_time = tctff5_mass["Time_Myr"][ref:end]/1000
 tctff5_late_gas = tctff5_mass["DiskGas_Msun"][ref:end]-tctff5_ref["DiskGas_Msun"]
 tctff5_late_form = tctff5_mass["FormedMass_Msun"][ref:end]-tctff5_ref["FormedMass_Msun"]
-tctff5_late_sfr = tctff5_sfh[(tctff5_sfh.index >= 2) & (tctff5_sfh.index <= 4)]
+tctff5_late_sfr = tctff5_sfh[(tctff5_sfh[:,0]/1000 >= 2) & (tctff5_sfh[:,0]/1000 <= 4)]
 
 tctff20_late_time = tctff20_mass["Time_Myr"][ref:end]/1000
 tctff20_late_gas = tctff20_mass["DiskGas_Msun"][ref:end]-tctff20_ref["DiskGas_Msun"]
 tctff20_late_form = tctff20_mass["FormedMass_Msun"][ref:end]-tctff20_ref["FormedMass_Msun"]
-tctff20_late_sfr = tctff20_sfh[(tctff20_sfh.index >= 2) & (tctff20_sfh.index <= 4)]
+tctff20_late_sfr = tctff20_sfh[(tctff20_sfh[:,0]/1000 >= 2) & (tctff20_sfh[:,0]/1000 <= 4)]
 
 linrot_late_time = linrot_mass["Time_Myr"][ref:end]/1000
 linrot_late_gas = linrot_mass["DiskGas_Msun"][ref:end]-linrot_ref["DiskGas_Msun"]
 linrot_late_form = linrot_mass["FormedMass_Msun"][ref:end]-linrot_ref["FormedMass_Msun"]
-linrot_late_sfr = linrot_sfh[(linrot_sfh.index >= 2) & (linrot_sfh.index <= 4)]
+linrot_late_sfr = linrot_sfh[(linrot_sfh[:,0]/1000 >= 2) & (linrot_sfh[:,0]/1000 <= 4)]
 
 norot_late_time = norot_mass["Time_Myr"][ref:end]/1000
 norot_late_gas = norot_mass["DiskGas_Msun"][ref:end]-norot_ref["DiskGas_Msun"]
 norot_late_form = norot_mass["FormedMass_Msun"][ref:end]-norot_ref["FormedMass_Msun"]
-norot_late_sfr = norot_sfh[(norot_sfh.index >= 2) & (norot_sfh.index <= 4)]
+norot_late_sfr = norot_sfh[(norot_sfh[:,0]/1000 >= 2) & (norot_sfh[:,0]/1000 <= 4)]
 
 
 # Plot late-time growth
@@ -331,109 +319,58 @@ norot_gas_rate = np.gradient(norot_late_gas, 5e7) + norot_form_rate
 
 fig, ax = plt.subplots(nrows=2, ncols=3, sharex=True, sharey=True, figsize=(12,5))
 
-ax[0,0].plot(fid_late_sfr, color='dimgray')
-ax[0,0].plot(fid_late_sfr.rolling(window=window).mean(), color='hotpink')
-ax[0,0].tick_params()
-ax_mass = ax[0,0].twinx()
-ax_mass.plot(fid_late_time, fid_gas_rate, lw=3)
-ax_mass.hlines(fid_gas_rate[ 0:20].mean(), 2, 3, color='C0', lw=1)
-ax_mass.hlines(fid_gas_rate[20:41].mean(), 3, 4, color='C0', lw=1)
-# ax_mass.hlines(fid_gas_rate[ 0:41].mean(), 2, 4, color='C0')
-ax_mass.axhline(0, color='k', ls='--')
-ax_mass.set_ylim(-2,2)
-ax[0,0].tick_params(which='both', left=False, right=True, labelleft=False, labelright=False)
-ax_mass.tick_params(which='both', left=True, right=False, labelleft=True, labelright=False)
-ax_mass.set_ylabel(r"$\Delta M_{\rm disk}$  [M$_\odot$ yr$^{-1}$]", labelpad=1.2, fontsize='large')
-ax_mass.yaxis.set_label_position("left")
+ax[0,0].plot(fid_late_sfr[:,0]/1000, fid_late_sfr[:,1], color='dimgray')
+ax[0,0].plot(fid_late_time, fid_gas_rate, lw=3)
+ax[0,0].hlines(fid_gas_rate[ 0:20].mean(), 2, 3, color='C0', lw=1)
+ax[0,0].hlines(fid_gas_rate[20:41].mean(), 3, 4, color='C0', lw=1)
+ax[0,0].axhline(0, color='k', ls='--')
+ax[0,0].set_ylabel(r"$\Delta M$  [M$_\odot$ yr$^{-1}$]", labelpad=1.2, fontsize='large')
 ax[0,0].set_title("Fiducial ", loc='right', y=0.85)
 
-ax[1,0].plot(cflow_late_sfr, color='dimgray')
-ax[1,0].plot(cflow_late_sfr.rolling(window=window).mean(), color='hotpink')
-ax[1,0].tick_params()
-ax_mass = ax[1,0].twinx()
-ax_mass.plot(cflow_late_time, cflow_gas_rate, lw=3, c='C3')
-ax_mass.hlines(cflow_gas_rate[ 0:20].mean(), 2, 3, color='C3', lw=1)
-ax_mass.hlines(cflow_gas_rate[20:41].mean(), 3, 4, color='C3', lw=1)
-# ax_mass.hlines(cflow_gas_rate[ 0:41].mean(), 2, 4, color='C3')
-ax_mass.axhline(0, color='k', ls='--')
-ax_mass.set_ylim(-2,2)
-ax[1,0].tick_params(which='both', left=False, right=True, labelleft=False, labelright=False)
-ax_mass.tick_params(which='both', left=True, right=False, labelleft=True, labelright=False)
-ax_mass.set_ylabel(r"$\Delta M_{\rm disk}$  [M$_\odot$ yr$^{-1}$]", labelpad=1.2, fontsize='large')
-ax_mass.yaxis.set_label_position("left")
+ax[1,0].plot(cflow_late_sfr[:,0]/1000, cflow_late_sfr[:,1], color='dimgray')
+ax[1,0].plot(cflow_late_time, cflow_gas_rate, lw=3, c='C3')
+ax[1,0].hlines(cflow_gas_rate[ 0:20].mean(), 2, 3, color='C3', lw=1)
+ax[1,0].hlines(cflow_gas_rate[20:41].mean(), 3, 4, color='C3', lw=1)
+ax[1,0].axhline(0, color='k', ls='--')
+ax[1,0].set_ylabel(r"$\Delta M$  [M$_\odot$ yr$^{-1}$]", labelpad=1.2, fontsize='large')
 ax[1,0].set_title("Cooling Flow ", loc='right', y=0.85)
 
-ax[0,1].plot(tctff5_late_sfr, color='dimgray')
-ax[0,1].plot(tctff5_late_sfr.rolling(window=window).mean(), color='hotpink')
-ax[0,1].tick_params()
-ax_mass = ax[0,1].twinx()
-ax_mass.plot(tctff5_late_time, tctff5_gas_rate, lw=3, c='C2')
-ax_mass.hlines(tctff5_gas_rate[ 0:20].mean(), 2, 3, color='C2', lw=1)
-ax_mass.hlines(tctff5_gas_rate[20:41].mean(), 3, 4, color='C2', lw=1)
-# ax_mass.hlines(tctff5_gas_rate[ 0:41].mean(), 2, 4, color='C2')
-ax_mass.axhline(0, color='k', ls='--')
-ax_mass.set_ylim(-2,2)
-ax[0,1].tick_params(which='both', left=False, right=True, labelleft=False, labelright=False)
-ax_mass.tick_params(which='both', left=True, right=False, labelleft=False, labelright=False)
+ax[0,1].plot(tctff5_late_sfr[:,0]/1000, tctff5_late_sfr[:,1], color='dimgray')
+ax[0,1].plot(tctff5_late_time, tctff5_gas_rate, lw=3, c='C2')
+ax[0,1].hlines(tctff5_gas_rate[ 0:20].mean(), 2, 3, color='C2', lw=1)
+ax[0,1].hlines(tctff5_gas_rate[20:41].mean(), 3, 4, color='C2', lw=1)
+ax[0,1].axhline(0, color='k', ls='--')
 ax[0,1].set_title(r"$t_{\rm c}/t_{\rm ff} = 5\ $ ", loc='right', y=0.85)
 
-ax[1,1].plot(tctff20_late_sfr, color='dimgray')
-ax[1,1].plot(tctff20_late_sfr.rolling(window=window).mean(), color='hotpink')
-ax[1,1].tick_params()
-ax_mass = ax[1,1].twinx()
-ax_mass.plot(tctff20_late_time, tctff20_gas_rate, lw=3, c='C1')
-ax_mass.hlines(tctff20_gas_rate[ 0:20].mean(), 2, 3, color='C1', lw=1)
-ax_mass.hlines(tctff20_gas_rate[20:41].mean(), 3, 4, color='C1', lw=1)
-# ax_mass.hlines(tctff20_gas_rate[ 0:41].mean(), 2, 4, color='C1')
-ax_mass.axhline(0, color='k', ls='--')
-ax_mass.set_ylim(-2,2)
-ax[1,1].tick_params(which='both', left=False, right=True, labelleft=False, labelright=False)
-ax_mass.tick_params(which='both', left=True, right=False, labelleft=False, labelright=False)
+ax[1,1].plot(tctff20_late_sfr[:,0]/1000, tctff20_late_sfr[:,1], color='dimgray')
+ax[1,1].plot(tctff20_late_time, tctff20_gas_rate, lw=3, c='C1')
+ax[1,1].hlines(tctff20_gas_rate[ 0:20].mean(), 2, 3, color='C1', lw=1)
+ax[1,1].hlines(tctff20_gas_rate[20:41].mean(), 3, 4, color='C1', lw=1)
+ax[1,1].axhline(0, color='k', ls='--')
 ax[1,1].set_title(r"$t_{\rm c}/t_{\rm ff} = 20\ $ ", loc='right', y=0.85)
 
-ax[0,2].plot(linrot_late_sfr, color='dimgray')
-ax[0,2].plot(linrot_late_sfr.rolling(window=window).mean(), color='hotpink')
-ax[0,2].tick_params()
-ax_mass = ax[0,2].twinx()
-ax_mass.plot(linrot_late_time, linrot_gas_rate, lw=3, c='C4')
-ax_mass.hlines(linrot_gas_rate[ 0:20].mean(), 2, 3, color='C4', lw=1)
-ax_mass.hlines(linrot_gas_rate[20:41].mean(), 3, 4, color='C4', lw=1)
-# ax_mass.hlines(linrot_gas_rate[ 0:41].mean(), 2, 4, color='C4')
-ax_mass.axhline(0, color='k', ls='--')
-ax_mass.set_ylim(-2,2)
-ax[0,2].tick_params(which='both', left=False, right=True, labelleft=False, labelright=True)
-ax_mass.tick_params(which='both', left=True, right=False, labelleft=False, labelright=False)
-ax[0,2].set_ylabel(r"SFR  [M$_\odot$ yr$^{-1}$]", labelpad=1.2, fontsize='large')
-ax[0,2].yaxis.set_label_position("right")
+ax[0,2].plot(linrot_late_sfr[:,0]/1000, linrot_late_sfr[:,1], color='dimgray')
+ax[0,2].plot(linrot_late_time, linrot_gas_rate, lw=3, c='C4')
+ax[0,2].hlines(linrot_gas_rate[ 0:20].mean(), 2, 3, color='C4', lw=1)
+ax[0,2].hlines(linrot_gas_rate[20:41].mean(), 3, 4, color='C4', lw=1)
+ax[0,2].axhline(0, color='k', ls='--')
 ax[0,2].set_title("Linear Rotation ", loc='right', y=0.85)
 
-ax[1,2].plot(norot_late_sfr, color='dimgray')
-ax[1,2].plot(norot_late_sfr.rolling(window=window).mean(), color='hotpink')
-ax[1,2].tick_params()
-ax_mass = ax[1,2].twinx()
-ax_mass.plot(norot_late_time, norot_gas_rate, lw=3, c='C5')
-ax_mass.hlines(norot_gas_rate[ 0:20].mean(), 2, 3, color='C5', lw=1)
-ax_mass.hlines(norot_gas_rate[20:41].mean(), 3, 4, color='C5', lw=1)
-# ax_mass.hlines(norot_gas_rate[ 0:41].mean(), 2, 4, color='C5')
-ax_mass.axhline(0, color='k', ls='--')
-ax_mass.set_ylim(-2,2)
-ax[1,2].tick_params(which='both', left=False, right=True, labelleft=False, labelright=True)
-ax_mass.tick_params(which='both', left=True, right=False, labelleft=False, labelright=False)
-ax[1,2].set_ylabel(r"SFR  [M$_\odot$ yr$^{-1}$]", labelpad=1.2, fontsize='large')
-ax[1,2].yaxis.set_label_position("right")
+ax[1,2].plot(norot_late_sfr[:,0]/1000, norot_late_sfr[:,1], color='dimgray')
+ax[1,2].plot(norot_late_time, norot_gas_rate, lw=3, c='C5')
+ax[1,2].hlines(norot_gas_rate[ 0:20].mean(), 2, 3, color='C5', lw=1)
+ax[1,2].hlines(norot_gas_rate[20:41].mean(), 3, 4, color='C5', lw=1)
+ax[1,2].axhline(0, color='k', ls='--')
 ax[1,2].set_title("No Rotation ", loc='right', y=0.85)
 
 ax[0,0].set_xlim(2, 4)
-ax[0,0].set_ylim(0,17)
+ax[0,0].set_ylim(-1.4,2.4)
 
+SFR = Line2D([0], [0], ls='-', c='dimgray')
+mdisk = Line2D([0], [0], ls='-', lw=3, c='C0')
 
-# ax[0,0].set_ylabel("SFR  [M$_\odot$ yr$^{-1}$]", labelpad=0.08)
-# ax[0,0].yaxis.set_label_position("right")
-
-ax[0,0].xaxis.set_minor_locator(MultipleLocator(0.1))
-
-ax[0,0].yaxis.set_major_locator(MultipleLocator(5))
-ax[0,0].yaxis.set_minor_locator(MultipleLocator(1))
+ax[0,0].legend([SFR,mdisk], ['SFR', r'$M_{\rm disk}$'],
+               ncol=2, loc="lower center")
 
 for i in range(3):
     ax[1,i].set_xlabel('$t$  [Gyr]', fontsize='large')
